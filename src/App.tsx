272f2352fb2a4856b3f743c9608d3bc28cfc39ff
sever.colhon1 <sever.colhon@inchcapeservices.com>
2024-04-18 07:46:08 +0300

    PK-1 Initial project setup and cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { PokemonClient } from 'pokenode-ts';
 
 function App() {
-  const [count, setCount] = useState(0)
+  (async () => {
+    const api = new PokemonClient();
+
+    await api
+      .getPokemonByName('luxray')
+      .then((data) => console.log(data)) // will output "Luxray"
+      .catch((error) => console.error(error));
+  })();
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="text-emerald-600">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
